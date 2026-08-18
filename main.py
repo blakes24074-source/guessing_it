4d10d028350b560f6b4c4d2d3c0c5c8734740264
blakes24074-source <blakes.24074@obhs.school.nz>
2026-08-19 09:10:57 +1200

    top comment added

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+    main.py
+        - who could guess what this file is?
+    iteration 01
+    loop
+        - intro - ask name
+        - mainloop
+            - generate correct answer
+            - loop until player answer = correct answer, give hints if wrong
+        - do not end mainloop
+    
+"""
+
+
